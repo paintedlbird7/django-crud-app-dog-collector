189229c20a2101bda7b2322be0a02353c932ee6c
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-22 15:58:15 -0700

    WIP: render and styled about page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
+# main_app/views.py
 from django.shortcuts import render
 
-# Create your views here.
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def about(request):
+    # return HttpResponse('<h1>About the DogCollector</h1>')
+    return render(request, 'about.html')
+
