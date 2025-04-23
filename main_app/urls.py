52e86bc8705ed277f5cd1d1f4f5af1e423a79fd5
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-22 20:58:24 -0700

    WIP: successfully submits a form

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # route for dogs index
     path('dogs/', views.dog_index, name='dog-index'),
     path('dogs/<int:dog_id>/', views.dog_detail, name='dog-detail'),
-
-
+    # new route used to create a dog
+    path('dogs/create/', views.DogCreate.as_view(), name='dog-create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 # main_app/views.py
 from django.shortcuts import render
 from .models import Dog
+from django.views.generic.edit import CreateView
+
 
 
 # Define the home view function
@@ -11,20 +13,6 @@ def about(request):
     # return HttpResponse('<h1>About the DogCollector</h1>')
     return render(request, 'about.html')
 
-# class Dog:
-#     def __init__(self, name, breed, description, age):
-#         self.name = name
-#         self.breed = breed
-#         self.description = description
-#         self.age = age
-
-# # Create a list of Dog instances
-# dogs = [
-#     Dog('Lolo', 'tabby', 'Kinda rude.', 3),
-#     Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-#     Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
-#     Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-# ]
 
 def dog_index(request):
     # Render the dogs/index.html template with the dogs data
@@ -34,3 +22,15 @@ def dog_index(request):
 def dog_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
+
+class DogCreate(CreateView):
+    model = Dog
+    # fields = '__all__'
+    fields = ['name', 'breed', 'description', 'age']
+    success_url = '/dogs/'
+
+
+#TODO: Create the template for creating cats
+
+
+
