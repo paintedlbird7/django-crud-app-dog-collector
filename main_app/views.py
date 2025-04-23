42512a70cff2df7c13be3b21f19cbb09b550dcfb
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-22 17:55:00 -0700

    WIP: using postgres to get dog data

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 # main_app/views.py
 from django.shortcuts import render
+from .models import Dog
+
 
 # Define the home view function
 def home(request):
@@ -9,23 +11,24 @@ def about(request):
     # return HttpResponse('<h1>About the DogCollector</h1>')
     return render(request, 'about.html')
 
-class Dog:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Dog:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-# Create a list of Cat instances
-dogs = [
-    Dog('Lolo', 'tabby', 'Kinda rude.', 3),
-    Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# # Create a list of Cat instances
+# dogs = [
+#     Dog('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
 
 def dog_index(request):
     # Render the dogs/index.html template with the dogs data
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
 
 #TODO: 🎓Left off at You Do: Create a template for the home page
